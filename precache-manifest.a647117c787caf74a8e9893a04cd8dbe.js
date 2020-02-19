e79e30c47eeb398ee4f7983a233892020fe9c16a
Kristian <mkristian@live.ru>
2020-02-19 11:27:26 +0400

    Updates

---
+++ precache-manifest.a647117c787caf74a8e9893a04cd8dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2668cafa4b602032bcfc8f5a7b5837e1",
+    "url": "/mkweather/index.html"
+  },
+  {
+    "revision": "666783745c829ab4bf61",
+    "url": "/mkweather/static/css/main.16448d33.chunk.css"
+  },
+  {
+    "revision": "56f08706e58969dafc2d",
+    "url": "/mkweather/static/js/2.afcf12c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mkweather/static/js/2.afcf12c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666783745c829ab4bf61",
+    "url": "/mkweather/static/js/main.20282c50.chunk.js"
+  },
+  {
+    "revision": "4570a9084b1bb9f5e415",
+    "url": "/mkweather/static/js/runtime-main.9ed55963.js"
+  }
+]);
